ae36962c6bfaf1db4c35cc4a23d2a197ebaa59ee
Carlos Bellete <dcarlosbellete@gmail.com>
2023-07-21 23:36:20 -0300

    Adicionar arquivos de projeto.

---
+++ ModeloTeste/Program.cs
@@ -0,0 +1,15 @@
+﻿using ModeloTeste.Services;
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CalculadoraImp c = new CalculadoraImp();
+
+        int num1 = 5;
+        int num2 = 10;
+
+        Console.WriteLine($"{num1} + {num2} = {c.Somar(num1, num2)}");
+    }
+}
